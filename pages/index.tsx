1f059ee52d54c0ce5a5b086bb9606e2905a387de
labelspiracy <labelspiracy@protonmail.com>
2022-10-20 17:03:48 +0200

    Add missing cities to map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import Head from 'next/head';
 
 import AvatarEditor from '../components/avatar-editor';
@@ -6,7 +6,7 @@ import Uploader from '../components/uploader';
 import EngageForm from '../components/engage-form';
 import Map from '../components/map';
 
-import { ModalProvider } from '../components/modal';
+import {ModalProvider} from '../components/modal';
 import constants from '../constants';
 
 export default function Index() {
@@ -24,12 +24,13 @@ export default function Index() {
                 <Head>
                     <title>Boycott</title>
                     <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width" />
+                    <meta name="viewport" content="width=device-width"/>
                     <link href='/favicon.svg' rel='icon'/>
 
                     <meta property='og:title' content='Je boycott la coupe du monde 2022 au Qatar'/>
                     <meta property='og:image' content='/thumbnail.jpg'/>
-                    <meta property='og:description' content='Parce que le monde du foot mérite mieux, je m’engage à ne pas regarder les matches de la Coupe du monde de football.'/>
+                    <meta property='og:description'
+                          content='Parce que le monde du foot mérite mieux, je m’engage à ne pas regarder les matches de la Coupe du monde de football.'/>
                     <meta property='og:url' content='//www.boycottqatar2022.fr'/>
                 </Head>
                 <header className="header">
@@ -80,7 +81,8 @@ export default function Index() {
                             </span>
                             <span className="horror-detail">
                                 Officiellement, <strong>3,6 millions de tonnes de CO2</strong> émises entre travaux
-                                et déplacements<sup><a href="#ref2">[2]</a></sup>. Une quantité fortement <strong>sous-estimée</strong>
+                                et déplacements<sup><a
+                                href="#ref2">[2]</a></sup>. Une quantité fortement <strong>sous-estimée</strong>
                                 <sup><a href="#ref3">[3</a>, <a href="#ref4">4]</a></sup>
                             </span>
                         </li>
@@ -103,7 +105,8 @@ export default function Index() {
                                 Des <strong>conditions de vie et de travail inhumaines</strong> entraînant blessures, handicap et décès non
                                 indemnisés<sup><a
                                 href="#ref8">[8]</a></sup>. Un système <strong>de tutelle, la « Kafala », apparenté à de l’esclavage moderne</strong>,
-                                officiellement aboli mais toujours en vigueur<sup><a href="#ref9">[9</a>, <a href="#ref10">10</a>, <a href="#ref11">11]</a></sup>
+                                officiellement aboli mais toujours en vigueur<sup><a href="#ref9">[9</a>, <a
+                                href="#ref10">10</a>, <a href="#ref11">11]</a></sup>
                             </span>
                         </li>
                     </ul>
@@ -117,7 +120,7 @@ export default function Index() {
                         Soit <strong>plus de 100 morts par match</strong>. 1 pour chaque minute jouée.
                     </h2>
                     <h2>
-                        Le monde du sport et ses passionné.e.s méritent bien mieux que cette Coupe du monde !
+                        Le monde du sport et ses passionné.e.s méritent bien mieux que cette Coupe du monde !
                     </h2>
                 </div>
 
@@ -141,40 +144,95 @@ export default function Index() {
                         <EngageForm/>
                     </div>
 
-                    <div className="personalities-container">
-                        <h3>
-                            Ces personnalités qui ont déjà appelé au boycott
-                        </h3>
-                        <ul className="personalities-list">
-                            {[
-                                { name: 'Régis Juanico', position: 'député de la Loire', link: 'https://www.lejdd.fr/Politique/jean-francois-debat-et-regis-juanico-boycotter-la-coupe-du-monde-au-qatar-pour-ne-pas-cautionner-4136381' },
-                                { name: 'Jean-François Debat', position: 'maire de Bourg-en-Bresse', link: 'https://www.lejdd.fr/Politique/jean-francois-debat-et-regis-juanico-boycotter-la-coupe-du-monde-au-qatar-pour-ne-pas-cautionner-4136381' },
-                                { name: 'Éric Cantona', position: 'ex-footballeur', link: 'https://www.eurosport.fr/football/coupe-du-monde/2022/eric-cantona-va-boycotter-la-coupe-du-monde-2022-au-qatar_sto9144232/story.shtml' },
-                                { name: 'Éric Piolle', position: 'maire de Grenoble', link: 'https://www.lefigaro.fr/sports/football/coupe-du-monde/fil-info/piolle-boycottons-la-coupe-du-monde-au-qatar-1038589' },
-                                { name: 'Vincent Lindon', position: 'acteur', link: 'https://www.lepoint.fr/people/un-asile-geant-vincent-lindon-pour-le-boycott-du-mondial-au-qatar-05-09-2022-2488537_2116.php' },
-                                { name: 'Philippe Lahm', position: 'ex-footballeur Bayern Munich', link: 'https://www.lequipe.fr/Football/Actualites/Philipp-lahm-boycottera-la-coupe-du-monde-au-qatar-pour-des-raisons-politiques/1347197' },
-                                { name: 'Alexis Corbière', position: 'député La Seine-Saint-Denis', link: 'https://www.lejdd.fr/Politique/alexis-corbiere-et-16-autres-deputes-insoumis-qatar-non-a-la-coupe-du-monde-immonde-4136203' },
-                                { name: 'Jeanne Barseghian', position: 'maire de Strasbourg', link: 'https://www.sofoot.com/boycott-du-mondial-la-ville-de-strasbourg-ne-diffusera-pas-de-matchs-sur-ecrans-geants-519478.html' },
-                                { name: 'Le Quotidien de la Réunion', position: '', link: 'https://www.francetvinfo.fr/coupe-du-monde/coupe-du-monde-2022-le-quotidien-de-la-reunion-boycotte-la-competition-au-qatar-et-assure-que-meme-si-la-france-est-championne-nous-ne-relaierons-pas-l-exploit-sportif_5359216.html' },
-                                { name: 'Raphaël Glucksmann', position: 'député européen', link: 'https://www.linkedin.com/posts/raphaelglucksmann_signez-la-p%C3%A9tition-activity-6982591437070606336--V8e/?originalSubdomain=be' },
-                                { name: 'Martine Aubry', position: 'maire de Lille', link: 'https://www.huffingtonpost.fr/politique/article/apres-strasbourg-lille-boycottera-la-coupe-du-monde-au-qatar_208430.html' },
-                                { name: 'Christian Teyssèdre', position: 'maire de Rodez', link: 'https://www.nouvelobs.com/societe/20221003.OBS64027/apres-lille-et-strasbourg-bordeaux-rodez-ces-villes-qui-boycottent-la-coupe-du-monde-au-qatar.html' },
-                            ].map(personality => (
-                                <li key={personality.name}>
-                                    <a href={personality.link} rel="external">
+                    <div className="boycott-vips">
+                        <div className="personalities-container">
+                            <h3>
+                                Ces personnalités qui ont déjà appelé au boycott
+                            </h3>
+                            <ul className="personalities-list">
+                                {[
+                                    {
+                                        name: 'Régis Juanico',
+                                        position: 'député de la Loire',
+                                        link: 'https://www.lejdd.fr/Politique/jean-francois-debat-et-regis-juanico-boycotter-la-coupe-du-monde-au-qatar-pour-ne-pas-cautionner-4136381'
+                                    },
+                                    {
+                                        name: 'Jean-François Debat',
+                                        position: 'maire de Bourg-en-Bresse',
+                                        link: 'https://www.lejdd.fr/Politique/jean-francois-debat-et-regis-juanico-boycotter-la-coupe-du-monde-au-qatar-pour-ne-pas-cautionner-4136381'
+                                    },
+                                    {
+                                        name: 'Éric Cantona',
+                                        position: 'ex-footballeur',
+                                        link: 'https://www.eurosport.fr/football/coupe-du-monde/2022/eric-cantona-va-boycotter-la-coupe-du-monde-2022-au-qatar_sto9144232/story.shtml'
+                                    },
+                                    {
+                                        name: 'Éric Piolle',
+                                        position: 'maire de Grenoble',
+                                        link: 'https://www.lefigaro.fr/sports/football/coupe-du-monde/fil-info/piolle-boycottons-la-coupe-du-monde-au-qatar-1038589'
+                                    },
+                                    {
+                                        name: 'Vincent Lindon',
+                                        position: 'acteur',
+                                        link: 'https://www.lepoint.fr/people/un-asile-geant-vincent-lindon-pour-le-boycott-du-mondial-au-qatar-05-09-2022-2488537_2116.php'
+                                    },
+                                    {
+                                        name: 'Philippe Lahm',
+                                        position: 'ex-footballeur Bayern Munich',
+                                        link: 'https://www.lequipe.fr/Football/Actualites/Philipp-lahm-boycottera-la-coupe-du-monde-au-qatar-pour-des-raisons-politiques/1347197'
+                                    },
+                                    {
+                                        name: 'Alexis Corbière',
+                                        position: 'député La Seine-Saint-Denis',
+                                        link: 'https://www.lejdd.fr/Politique/alexis-corbiere-et-16-autres-deputes-insoumis-qatar-non-a-la-coupe-du-monde-immonde-4136203'
+                                    },
+                                    {
+                                        name: 'Jeanne Barseghian',
+                                        position: 'maire de Strasbourg',
+                                        link: 'https://www.sofoot.com/boycott-du-mondial-la-ville-de-strasbourg-ne-diffusera-pas-de-matchs-sur-ecrans-geants-519478.html'
+                                    },
+                                    {
+                                        name: 'Le Quotidien de la Réunion',
+                                        position: '',
+                                        link: 'https://www.francetvinfo.fr/coupe-du-monde/coupe-du-monde-2022-le-quotidien-de-la-reunion-boycotte-la-competition-au-qatar-et-assure-que-meme-si-la-france-est-championne-nous-ne-relaierons-pas-l-exploit-sportif_5359216.html'
+                                    },
+                                    {
+                                        name: 'Raphaël Glucksmann',
+                                        position: 'député européen',
+                                        link: 'https://www.linkedin.com/posts/raphaelglucksmann_signez-la-p%C3%A9tition-activity-6982591437070606336--V8e/?originalSubdomain=be'
+                                    },
+                                    {
+                                        name: 'Martine Aubry',
+                                        position: 'maire de Lille',
+                                        link: 'https://www.huffingtonpost.fr/politique/article/apres-strasbourg-lille-boycottera-la-coupe-du-monde-au-qatar_208430.html'
+                                    },
+                                    {
+                                        name: 'Christian Teyssèdre',
+                                        position: 'maire de Rodez',
+                                        link: 'https://www.nouvelobs.com/societe/20221003.OBS64027/apres-lille-et-strasbourg-bordeaux-rodez-ces-villes-qui-boycottent-la-coupe-du-monde-au-qatar.html'
+                                    },
+                                ].map(personality => (
+                                    <li key={personality.name}>
+                                        <a href={personality.link} rel="external">
                                             <span className="personality-name">
                                                 {personality.name}
                                             </span>
-                                        <span className="personality-position">
+                                            <span className="personality-position">
                                                 {personality.position}
                                             </span>
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                        <figure className="map">
-                            <Map />
-                        </figure>
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="map-container">
+                            <h3>
+                                Ces villes qui ne diffuseront pas les matchs
+                            </h3>
+                            <figure className="map">
+                                <Map/>
+                            </figure>
+                        </div>
                     </div>
 
                     <div className="block-action action-second" id="profile">
@@ -187,7 +245,9 @@ export default function Index() {
                                     Crée ta photo de profil
                                 </h2>
                                 <p>
-                                    Je montre mon désaccord en téléchargeant un bandeau «&nbsp;{constants.hashtag}&nbsp;» et j’actualise ma photo de profil sur les réseaux sociaux&nbsp;:
+                                    Je montre mon désaccord en téléchargeant un bandeau
+                                    «&nbsp;{constants.hashtag}&nbsp;» et j’actualise ma photo de profil sur les réseaux
+                                    sociaux&nbsp;:
                                 </p>
                             </div>
                             <div>
@@ -218,10 +278,16 @@ export default function Index() {
                         </p>
                         <ul>
                             <li>
-                                Boycotter les sponsors et partenaires de l’événement et ne pas consommer ou acheter leurs produits : McDonalds, Coca-Cola, Budweiser, Adidas, Hyundai, KIA, Qatar Airways, VISA, Crypto.com, BYJU&apos;S, Wanda Group, Hisense, VIVO, Mengniu.
+                                Boycotter les sponsors et partenaires de l’événement et ne pas consommer ou acheter
+                                leurs produits : McDonalds, Coca-Cola, Budweiser, Adidas, Hyundai, KIA, Qatar Airways,
+                                VISA, Crypto.com, BYJU&apos;S, Wanda Group, Hisense, VIVO, Mengniu.
                             </li>
                             <li>
-                                Signer des pétitions, comme <a href="https://www.amnesty.fr/petitions/coupe-du-monde-qatar-2022-fff?utm_medium=boycottqatar2022.fr">celle d’Amnesty International</a> et celle de <a href="https://www.change.org/p/m-le-pr%C3%A9sident-ne-participez-pas-%C3%A0-la-coupe-du-monde-au-qatar">Raphaël Glucksmann</a>
+                                Signer des pétitions, comme <a
+                                href="https://www.amnesty.fr/petitions/coupe-du-monde-qatar-2022-fff?utm_medium=boycottqatar2022.fr">celle
+                                d’Amnesty International</a> et celle de <a
+                                href="https://www.change.org/p/m-le-pr%C3%A9sident-ne-participez-pas-%C3%A0-la-coupe-du-monde-au-qatar">Raphaël
+                                Glucksmann</a>
                             </li>
                             <li>
                                 Passer à l’action avec <a href="https://carton-rouge-qatar-2022.org/">Carton Rouge</a>
@@ -234,7 +300,8 @@ export default function Index() {
                         </h2>
                         <p>
                             Que tu fasses un foot entre potes, une soirée film, un gros jeu de société, une bonne grosse
-                            tartiflette, montre au monde que la vie continue aussi sans la Coupe du monde avec le hashtag <strong>#HappyBoycott</strong> !
+                            tartiflette, montre au monde que la vie continue aussi sans la Coupe du monde avec le
+                            hashtag <strong>#HappyBoycott</strong> !
                         </p>
                     </div>
                 </div>
@@ -257,7 +324,7 @@ export default function Index() {
                             'https://www.mediapart.fr/journal/international/220922/au-qatar-l-enfer-des-travailleuses-domestiques',
                             'https://www.theguardian.com/global-development/2022/sep/20/workers-qatar-world-cup-football-stadiums-toil-in-debt-and-squalor?amp;amp;amp'
                         ].map((link, index) => (
-                            <li id={`ref${index+1}`} key={link}>
+                            <li id={`ref${index + 1}`} key={link}>
                                 <a
                                     href={link}
                                     rel="external"
